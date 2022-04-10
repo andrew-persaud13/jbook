9d9a331d073546fa885dae59bff8c17f715ad101
LAPTOP-D7FTRIR6\andre <andrew.persaud.dev@gmail.com>
2022-04-10 18:31:34 -0400

    Added monaco editor and re-factored index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,32 @@
-import * as esbuild from 'esbuild-wasm';
-
 import ReactDOM from 'react-dom';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/CodeEditor';
+
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import Preview from './components/Preview';
+import bundlerService from './bundler';
 
 const App = () => {
-  const serviceRef = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const startService = async () => {
-    //esbuild will be running in the browser, cant access file system
-    //intercept when it tries to look, and find the module ourselves
-    serviceRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
   const onClick = async () => {
-    if (!serviceRef.current) return;
-
-    const result = await serviceRef.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': 'production',
-        global: 'window',
-      },
-    });
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    const code = await bundlerService(input);
+    setCode(code);
   };
+
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      <CodeEditor
+        initialValue='Hello Editor!'
+        onChange={(value) => setInput(value)}
+      />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe src='/test.html' />
+
+      <Preview code={code} />
     </div>
   );
 };
